6cfcc65d2b6c0af3ec3e8c265d0022c3032843ff
meincrab <meincrab@gmail.com>
2019-10-23 07:26:36 +0300

    Gitlab -> github

--- build/precache-manifest.bbfa19d48bb1e0776304c1fec53a8d9d.js
+++ build/precache-manifest.bbfa19d48bb1e0776304c1fec53a8d9d.js
@@ -1,47 +1,27 @@
-self.__precacheManifest = [
+self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "dea7a5567d4390f55ba6084d6db42a5b",
-    "url": "/meincrab/portfolio/static/media/demo_asp.dea7a556.png"
+    "revision": "2bf8663ef8214a1688cc585101adfb62",
+    "url": "/meincrab/portfolio/index.html"
   },
   {
-    "revision": "e169935de425d968aa1a",
-    "url": "/meincrab/portfolio/static/css/main.803ec0cd.chunk.css"
+    "revision": "05e4aa761fb04c1ed8fa",
+    "url": "/meincrab/portfolio/static/css/2.356768eb.chunk.css"
   },
   {
-    "revision": "378e7e3a1fb480436920",
-    "url": "/meincrab/portfolio/static/js/runtime~main.115d1e8b.js"
+    "revision": "f522cdf1666f91f648d1",
+    "url": "/meincrab/portfolio/static/css/main.31ed8614.chunk.css"
   },
   {
-    "revision": "47aebea88a8f26fb1e5e47804aaa83e4",
-    "url": "/meincrab/portfolio/static/media/smalldeer.47aebea8.svg"
+    "revision": "05e4aa761fb04c1ed8fa",
+    "url": "/meincrab/portfolio/static/js/2.a3438d5b.chunk.js"
   },
   {
-    "revision": "5b78eaf2ca99e4f3202f",
-    "url": "/meincrab/portfolio/static/js/2.e25de61c.chunk.js"
+    "revision": "f522cdf1666f91f648d1",
+    "url": "/meincrab/portfolio/static/js/main.98852627.chunk.js"
   },
   {
-    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
-    "url": "/meincrab/portfolio/static/media/logo.ee7cd8ed.svg"
-  },
-  {
-    "revision": "76b035278bad6d7d8bb5c77464d36cfc",
-    "url": "/meincrab/portfolio/static/media/pictureMe.76b03527.jpeg"
-  },
-  {
-    "revision": "ced611daf7709cc778da928fec876475",
-    "url": "/meincrab/portfolio/static/media/slick.ced611da.eot"
-  },
-  {
-    "revision": "f97e3bbf73254b0112091d0192f17aec",
-    "url": "/meincrab/portfolio/static/media/slick.f97e3bbf.svg"
-  },
-  {
-    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
-    "url": "/meincrab/portfolio/static/media/slick.b7c9e1e4.woff"
-  },
-  {
-    "revision": "d41f55a78e6f49a5512878df1737e58a",
-    "url": "/meincrab/portfolio/static/media/slick.d41f55a7.ttf"
+    "revision": "0e68d1ad2f16108fd9f7",
+    "url": "/meincrab/portfolio/static/js/runtime-main.8f6672b7.js"
   },
   {
     "revision": "f079d7c357e2dd6fb2dd5c60f0b0ac17",
@@ -64,8 +44,8 @@ self.__precacheManifest = [
     "url": "/meincrab/portfolio/static/media/boat_game_asp.76faf2d9.png"
   },
   {
-    "revision": "e169935de425d968aa1a",
-    "url": "/meincrab/portfolio/static/js/main.f7fba28f.chunk.js"
+    "revision": "dea7a5567d4390f55ba6084d6db42a5b",
+    "url": "/meincrab/portfolio/static/media/demo_asp.dea7a556.png"
   },
   {
     "revision": "502ccf90aed8cb3da81ae0f1e1bdda07",
@@ -74,6 +54,10 @@ self.__precacheManifest = [
   {
     "revision": "54db2d7002c35fff87fbad0cc28074b2",
     "url": "/meincrab/portfolio/static/media/eagle.54db2d70.jpg"
+  },
+  {
+    "revision": "071c57c9555845b409eca7d7eb711ee5",
+    "url": "/meincrab/portfolio/static/media/favicon.071c57c9.ico"
   },
   {
     "revision": "cf2b67cd0ddf9cb3fd2db4ed5794f365",
@@ -100,6 +84,10 @@ self.__precacheManifest = [
     "url": "/meincrab/portfolio/static/media/olio_solitaire.33570baa.PNG"
   },
   {
+    "revision": "76b035278bad6d7d8bb5c77464d36cfc",
+    "url": "/meincrab/portfolio/static/media/pictureMe.76b03527.jpeg"
+  },
+  {
     "revision": "c9040bae57a47a794674611e7da66746",
     "url": "/meincrab/portfolio/static/media/pretzels.c9040bae.PNG"
   },
@@ -112,6 +100,26 @@ self.__precacheManifest = [
     "url": "/meincrab/portfolio/static/media/scheduler.08e0318e.png"
   },
   {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/meincrab/portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/meincrab/portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/meincrab/portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/meincrab/portfolio/static/media/slick.f97e3bbf.svg"
+  },
+  {
+    "revision": "31e7b1242fe130f9b46a02de9d3bb4cf",
+    "url": "/meincrab/portfolio/static/media/smalldeer.31e7b124.svg"
+  },
+  {
     "revision": "0c39a12acc73b51f432f03b53e6ea4ef",
     "url": "/meincrab/portfolio/static/media/solitaire_asp.0c39a12a.png"
   },
@@ -122,13 +130,5 @@ self.__precacheManifest = [
   {
     "revision": "ab029e43cd45e0d9dd4fcedab172e7ee",
     "url": "/meincrab/portfolio/static/media/survival_shooter.ab029e43.PNG"
-  },
-  {
-    "revision": "5b78eaf2ca99e4f3202f",
-    "url": "/meincrab/portfolio/static/css/2.b2e0c931.chunk.css"
-  },
-  {
-    "revision": "1cfbb0e4591f98aaebe7100cae6ddc4f",
-    "url": "/meincrab/portfolio/index.html"
   }
-];
+]);
--- src/Footer.js
+++ src/Footer.js
@@ -11,7 +11,7 @@ class Footer extends Component {
         <div className="contactInfo">
             <a href="https://www.linkedin.com/in/andreev-alexander/"><h3>Linkedin</h3></a>
             <br></br>
-            <a href="https://github.com/meincrab/"><h3>GitLab</h3></a>     
+            <a href="https://github.com/meincrab/"><h3>Github</h3></a>     
             <br></br>
             <a href="https://drive.google.com/file/d/13nso6K5BT3ykeRWanOYPJjJkJAA71yR0/view?usp=sharing"><h3>CV</h3></a>     
         </div>
